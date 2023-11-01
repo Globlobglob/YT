42b8589d1e16c3cd56b6602b81cc92a0e4794152
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-11-02 00:29:05 +0200

    Add files via upload

---
+++ тема 6 компьютерный практикум/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.Write("ввести число окончания отсчета");
+             int x = Convert.ToInt32(Console.ReadLine());    
+
+             for(int i = 0; i <= x; i++)
+             {
+                 Console.Write( i );
+
+             }*/
+
+
+            /*
+            for(int x = 10; x <= 99; x++)
+            {
+
+                if (x % 2==0)
+                {
+                    Console.WriteLine(x);
+                }
+
+            }*/
+
+
+            /*
+            for (double x = -5.0; x <= 5.0; x +=0.2)
+            {
+                double y = 2 * x * x + 4 * x - 5;
+                Console.WriteLine($"{x}\t   {y}");
+            }
+            */
+
+            /*int summ = 0;
+
+            for(int i = 0; i <= 1000; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    summ += i;
+                    Console.WriteLine(summ);
+                }
+            }
+            */
+
+            /*
+            Console.Write("enter a number");
+            int number=Convert.ToInt32(Console.ReadLine());
+            string numberString = number.ToString(); 
+            int x = numberString.Length;
+            Console.Write(x);*/
+
+
+
+        }
+    }
+}
