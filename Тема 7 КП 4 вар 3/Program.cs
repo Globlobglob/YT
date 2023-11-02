603b574154ebd1e2794f0efb88dca469fd53dff5
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-11-02 09:58:30 +0200

    Add files via upload

---
+++ Тема 7 КП 4 вар 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("enter a sentences");
+            string s = Console.ReadLine();
+            Console.WriteLine("enter a symbol to  del");
+            char x =Convert.ToChar(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder(s);
+
+            for (int i = 0; i < sb.Length;)
+            {
+                if (sb[i] == x)
+                {
+                    sb.Remove(i, 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            string result = sb.ToString();
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
