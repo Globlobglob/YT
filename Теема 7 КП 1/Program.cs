2e1d9a1d35a2b91d8f053ffe6a79270229f00fd2
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-11-02 08:14:44 +0200

    Add files via upload

---
+++ Теема 7 КП 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              
+
+
+            /*for(int i = 0; i <= 256; i++)
+
+            {
+
+                Console.Write($"{i} =symbol {(char)i}");
+
+            }*/
+
+            /*Console.Write("ВВедите symbol ");
+
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine($"Symbol{(int)ch}");*/
+
+
+            /*Console.Write("ВВедите symbol ");
+
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine($"Symbol= {(char)(int)ch+1}");
+
+            Console.WriteLine($"Symbol= {(char)(int)ch + 2}");*/
+
+
+
+            /*Console.Write("Enter a word= ");
+
+            string ch = (Console.ReadLine());
+
+            char[] omas = ch.ToCharArray();
+
+            for(int i = omas.Length-1; i >= 0 ; i--)
+
+            {
+
+                Console.WriteLine($"{omas[i]}");
+
+            }*/
+
+
+            /*Console.Write("Enter a word= ");
+
+            string ch = (Console.ReadLine());
+
+            Console.WriteLine(ch.ToUpper());
+
+            Console.WriteLine(ch.ToLower());
+
+            Console.ReadKey();*/
+
+
+            /* foreach(char i in omas)
+
+             {
+
+                 Console.WriteLine(i);*/
+
+          
+
+        }
+    }
+}
