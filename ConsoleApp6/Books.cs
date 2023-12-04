ef28f16bb052c7a351fe7aa27f5aae3b0e0faf44
Globlobglob <122725189+Globlobglob@users.noreply.github.com>
2023-12-04 10:55:01 +0200

    Add files via upload

---
+++ ConsoleApp6/Books.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp5;
+
+
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static Student GetStudent()
+        {
+
+            Student student = new Student();    
+            student.id=Guid.NewGuid();
+            student.name = "Igorevich";
+            student.age = 10;
+
+            return student;
+        }
+       static void Print(Student student)
+        {
+            Console.WriteLine(student.id);
+            Console.WriteLine(student.name);
+                Console.WriteLine(student.age);
+            Console.WriteLine(student.surname);
+
+
+        }
+        static void Main(string[] args)
+        {
+            
+            var student =GetStudent();
+            Print(student);
+
+
+
+
+            ReadKey();
+
+
+
+        }
+       
+    }
+    
+}
---
+++ ConsoleApp6/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Student
+    {
+        public Guid id;
+        public string name;
+        public string surname;
+        public int age;
+
+    }
+
+}
